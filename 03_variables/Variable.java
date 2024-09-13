8c77765821b20e2db011551427eeaa8426235db4
BuddhadebKoner <jeetkoner36@gmail.com>
2024-09-13 12:44:48 +0530

    what is variables

---
+++ 03_variables/Variable.java
@@ -0,0 +1,25 @@
+public class Variable {
+  public static void main(String[] args) {
+    // Integer variable
+    int myNumber = 88;
+
+    // Boolean variable
+    boolean isFun = true;
+
+    // Character variable
+    char movieRating = 'A';
+
+    // Double variable
+    double myDecimal = 89.9;
+
+    // String variable
+    String message = "Hello World";
+
+    // Print variables
+    System.out.println(myNumber);
+    System.out.println(isFun);
+    System.out.println(movieRating);
+    System.out.println(myDecimal);
+    System.out.println(message);
+  }
+}
